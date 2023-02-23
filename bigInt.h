3791045964c9bb01efce965c109f6576d353d6de
cpokam <cpokam@gmail.com>
2023-02-23 13:24:16 +0100

    first commit

---
+++ bigInt.h
@@ -0,0 +1,76 @@
+// BigInt (BIG INTEGERS) in C++
+// C++ program to implement
+// the above approach
+#include <string>
+using namespace std;
+
+class BigInt{
+	string digits;
+public:
+
+    //Constructors, allow implicit conversions
+    BigInt(unsigned long long n = 0);
+	BigInt(const string &);
+	BigInt(const char *);
+	BigInt(const BigInt &);
+
+	//Helper Functions:
+	friend void divide_by_2(BigInt &a);
+	friend bool Null(const BigInt &);
+	friend int Length(const BigInt &);
+	int operator[](const int)const;
+
+    /* * * * Operator Overloading * * * */
+
+	//Direct assignment
+	BigInt &operator=(const BigInt &);
+
+	//Post/Pre - Incrementation
+	BigInt &operator++();
+	BigInt operator++(int temp);
+	BigInt &operator--();
+	BigInt operator--(int temp);
+
+	//Addition and Subtraction
+	friend BigInt &operator+=(BigInt &, const BigInt &);
+	friend BigInt operator+(const BigInt &, const BigInt &);
+	friend BigInt operator-(const BigInt &, const BigInt &);
+	friend BigInt &operator-=(BigInt &, const BigInt &);
+
+	//Comparison operators
+	friend bool operator==(const BigInt &, const BigInt &);
+	friend bool operator!=(const BigInt &, const BigInt &);
+
+	friend bool operator>(const BigInt &, const BigInt &);
+	friend bool operator>=(const BigInt &, const BigInt &);
+	friend bool operator<(const BigInt &, const BigInt &);
+	friend bool operator<=(const BigInt &, const BigInt &);
+
+	//Multiplication and Division
+	friend BigInt &operator*=(BigInt &, const BigInt &);
+	friend BigInt operator*(const BigInt &, const BigInt &);
+	friend BigInt &operator/=(BigInt &, const BigInt &);
+	friend BigInt operator/(const BigInt &, const BigInt &);
+
+	//Modulo
+	friend BigInt operator%(const BigInt &, const BigInt &);
+	friend BigInt &operator%=(BigInt &, const BigInt &);
+
+	//Power Function
+	friend BigInt &operator^=(BigInt &,const BigInt &);
+	friend BigInt operator^(BigInt &, const BigInt &);
+
+	//Read and Write
+	friend ostream &operator<<(ostream &,const BigInt &);
+	friend istream &operator>>(istream &, BigInt &);
+
+    //Square Root Function
+    friend BigInt sqrt(BigInt &a);
+
+	//Others
+	friend BigInt NthCatalan(int n);
+	friend BigInt NthFibonacci(int n);
+	friend BigInt Factorial(int n);
+};
+
+
